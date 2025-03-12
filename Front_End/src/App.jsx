e19630c06d65b8ed60e00cc3b537487c04de2c58
Ved Kumar <vedkumar739@gmail.com>
2025-03-12 05:53:23 +0530

    second lasttt

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -5,7 +5,7 @@ import { AuthPage } from './page/AuthPage'
 import { ProductPage } from './page/ProductPage'
 import { ProductListed } from './Component/ProductListed'
 import { UserPage } from './page/UserPage' 
-import { UserListed } from './Component/userListed'
+import { UserListed } from './Component/UserListed'
 import { Purchese } from './Component/Purchese'
 
 
